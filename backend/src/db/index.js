6e3fc17daadb59b1e56c8ad90bd6415b845173b5
Safal2004 <safalgawande2004@gmail.com>
2026-03-29 14:39:01 +0530

    fixed connection

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,12 +2,12 @@ import "dotenv/config";
 import pg from "pg";
 const { Pool } = pg;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database:process.env.DB_NAME,
-  password:String(process.env.DB_PASSWORD),
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
+

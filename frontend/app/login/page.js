9b325465c5ae134a87c53812d1f8f0032b7ec21d
Safal2004 <safalgawande2004@gmail.com>
2026-04-04 20:21:25 +0530

    redirects to aws backend server

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -10,7 +10,7 @@ export default function Login() {
     const router = useRouter();
 
     const login = async () => {
-        const res = await fetch("http://35.154.150.20:4000/auth/login", {
+        const res = await fetch("https://api.safaldev.me/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -10,7 +10,7 @@ export default function Register() {
     const router = useRouter();
 
     const register = async () => {
-        const res = await fetch("http://35.154.150.20:4000/auth/register", {
+        const res = await fetch("https://api.safaldev.me/auth/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
